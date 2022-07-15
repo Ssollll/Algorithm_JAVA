c05d733a4de46489dfd853871ff7a6482bd65f30
Hansol Jang <ssoooooll@naver.com>
2022-07-15 19:33:51 +0900

    boj_1008

---
+++ BAEKJOON_Algorithm/src/boj_1008.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class boj_1008 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		Double A = scan.nextDouble();
+		Double B = scan.nextDouble();
+	
+		System.out.println(A / B);
+	}
+
+}
