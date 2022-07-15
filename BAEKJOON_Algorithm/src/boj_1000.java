04c1256350811d4296b9923a403864a8d9c7b5ba
Hansol Jang <ssoooooll@naver.com>
2022-07-15 19:25:19 +0900

    boj_1000

---
+++ BAEKJOON_Algorithm/src/boj_1000.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class boj_1000 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		int A = scan.nextInt();
+		int B = scan.nextInt();
+		
+		System.out.println(A+B);
+	}
+
+}
