9c029c557ef6ef5534d953f633068adfee2bf155
Hansol Jang <ssoooooll@naver.com>
2022-07-15 19:25:36 +0900

    boj_1001

---
+++ BAEKJOON_Algorithm/src/boj_1001.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class boj_1001 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		int A = scan.nextInt();
+		int B = scan.nextInt();
+		
+		System.out.println(A-B);
+	}
+
+}
