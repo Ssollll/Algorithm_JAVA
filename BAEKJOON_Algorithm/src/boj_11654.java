a9d73c37cc58caa047d0492b6754e939bc486132
Hansol Jang <ssoooooll@naver.com>
2022-07-18 00:48:46 +0900

    boj_11654

---
+++ BAEKJOON_Algorithm/src/boj_11654.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class boj_11654 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println((int)scan.next().charAt(0));
+	}
+
+}
