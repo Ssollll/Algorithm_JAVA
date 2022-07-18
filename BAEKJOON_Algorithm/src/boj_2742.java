6611612fe8d1481029e677e6bbc7d0f7fad86c27
Hansol Jang <ssoooooll@naver.com>
2022-07-18 18:19:18 +0900

    boj_2742

---
+++ BAEKJOON_Algorithm/src/boj_2742.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class boj_2742 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		int N = scan.nextInt();
+		
+		for(int i = N; i > 0; i--) {
+			System.out.println(i);
+		}
+	}
+
+}
