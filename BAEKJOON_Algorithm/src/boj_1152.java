bcf2e245a1f8608e23334224d64d63ffbc244cc7
Hansol Jang <ssoooooll@naver.com>
2022-07-19 16:34:03 +0900

    boj_1152_edit

--- BAEKJOON_Algorithm/src/boj_1152.java
+++ BAEKJOON_Algorithm/src/boj_1152.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.*;
 
 public class boj_1152 {
 
@@ -8,8 +9,15 @@ public class boj_1152 {
 
 		String s = br.readLine();
 
-		String[] sarr = s.split(" "); // 입력받은 문자열 공백기준 자르기
+		StringTokenizer st = new StringTokenizer(s, " ");
+		
+		System.out.println(st.countTokens()); // 토큰의 갯수 세기
+		
 
-		System.out.println(sarr.length);
+//		String[] sarr = s.split(" "); // 입력받은 문자열 공백기준 자르기
+		// 단순 공백으로 자르면 문자열 앞 뒤의 공백도 같이 계산되기에 split 함수는 이 문제에 적합하지 않음
+		// StringTokenizer을 사용해야 함
+
+//		System.out.println(sarr.length);
 	}
 }
